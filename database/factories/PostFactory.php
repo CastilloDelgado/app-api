d161d78f3e665b4d9d6c740758c671ba7b638ad5
Marco Castillo <marco.castillo@emergys.com>
2023-02-02 15:23:08 -0600

    Project created with laravel new command

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => fake()->sentence(10),
+            'description' => fake()->sentence(10),
+            'location' => fake()->sentence(5),
+            'date' => fake()->date()
+        ];
+    }
+}
